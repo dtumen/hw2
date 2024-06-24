ac97a98515b956ca9b8944bae0e3f612b9618e10
dtumen <dt1109@mail.ru>
2024-06-24 23:28:40 +0200

    hw6 feat: add hw6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,13 +19,14 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        let result = restoreState('hw6-editable-span-value', '');
+        setValue(result);
     }
 
     return (
         <div id={'hw6'}>
             <div className={s2.hwTitle}>Homework #6</div>
-
+            {/*<hr/>*/}
             {/*демонстрация возможностей компоненты:*/}
             <div className={s2.hw}>
                 <div className={s.editableSpanContainer}>
