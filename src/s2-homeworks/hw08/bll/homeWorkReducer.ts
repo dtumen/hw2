5bcebe5b5a5c8e26d7bd3c42e4416bc70d9425fa
dtumen <dt1109@mail.ru>
2024-07-01 11:47:45 +0200

    hw8 feat: homeWorkReducer is done with passed tests

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,23 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            const stateCopy = [...state];
 
-            return state // need to fix
+            if (action.payload === 'up') {
+                stateCopy.sort((a, b) => a.name.localeCompare(b.name));
+            } else {
+                stateCopy.sort((a, b) => b.name.localeCompare(a.name));
+            }
+
+            return stateCopy // need to fix
         }
         case 'check': {
+            let adultUsers = state.filter((u) => u.age > 17);
 
-            return state // need to fix
+            return adultUsers // need to fix
         }
         default:
             return state
